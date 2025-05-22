2ebe5bc8fccb2d7f7bdc367239d2120fbcaf6a8d
Milo Vingoe <milovingoe@hotmail.com>
2025-05-22 17:06:10 +0100

    Bookmark delete route, method and view amended

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :lists, only: [:index, :show, :new, :create ] do
-    resources :bookmarks, only: [:new, :create, :destroy]
+    resources :bookmarks, only: [:new, :create]
   end
+  resources :bookmarks, only: :destroy
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
