7eabea93061527ac6ccddbcb4970625301b44a4c
Milo Vingoe <milovingoe@hotmail.com>
2025-05-23 16:44:16 +0100

    Navbar and banner HTML and styling added

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :lists, only: [:index, :show, :new, :create ] do
+  resources :lists, only: [ :show, :new, :create ] do
     resources :bookmarks, only: [:new, :create]
   end
   resources :bookmarks, only: :destroy
@@ -9,5 +9,5 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  root "lists#index"
 end
